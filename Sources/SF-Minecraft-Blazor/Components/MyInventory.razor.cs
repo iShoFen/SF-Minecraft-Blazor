8d57db3ed1e8573b0aca69480758d383ce3bb2c4
Florent MARQUES <florent.marques@etu.uca.fr>
2023-02-26 22:13:09 +0100

    :sparkles: add my inventory

---
+++ Sources/SF-Minecraft-Blazor/Components/MyInventory.razor.cs
@@ -0,0 +1,6 @@
+namespace SF_Minecraft_Blazor.Components;
+
+public partial class MyInventory
+{
+    
+}
---
+++ Sources/SF-Minecraft-Blazor/Pages/Inventory.razor.cs
@@ -0,0 +1,74 @@
+using Blazorise.DataGrid;
+using SF_Minecraft_Blazor.Data;
+
+namespace SF_Minecraft_Blazor.Pages;
+
+public partial class Inventory
+{
+    private List<Item> Items { get; set; } = new()
+    {
+        new Item
+        {
+            Id = 1,
+            DisplayName = "Diamond",
+            Name = "item1",
+            CreatedDate = DateTime.Now,
+            EnchantCategories = new List<string>(),
+            RepairWith = new List<string>(),
+            MaxDurability = 4,
+            StackSize = 45
+        },
+        new Item
+        {
+            Id = 1,
+            DisplayName = "Dirt",
+            Name = "item1",
+            CreatedDate = DateTime.Now,
+            EnchantCategories = new List<string>(),
+            RepairWith = new List<string>(),
+            MaxDurability = 4,
+            StackSize = 45
+        },
+        new Item
+        {
+            Id = 1,
+            DisplayName = "Stone",
+            Name = "item1",
+            CreatedDate = DateTime.Now,
+            EnchantCategories = new List<string>(),
+            RepairWith = new List<string>(),
+            MaxDurability = 4,
+            StackSize = 45
+        }
+    };
+
+    private int totalItems { get; set; }
+
+    private string SearchValue { get; set; } = "";
+
+    private DataGrid<Item> _dataGrid = null!;
+
+    public Inventory()
+    {
+        totalItems = Items.Count;
+    }
+
+    private Task OnReadData(DataGridReadDataEventArgs<Item> arg)
+    {
+        return Task.CompletedTask;
+    }
+
+    private Task OnSearchRequested(string search)
+    {
+        SearchValue = search;
+        return _dataGrid.Reload();
+    }
+
+    private bool OnCustomFilter(Item item)
+    {
+        if (string.IsNullOrEmpty(SearchValue))
+            return true;
+        return item.DisplayName.Contains(SearchValue, StringComparison.OrdinalIgnoreCase)
+               || item.Name.Contains(SearchValue, StringComparison.OrdinalIgnoreCase);
+    }
+}
